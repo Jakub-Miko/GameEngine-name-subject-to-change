4c5200c67401adeb3d9107ea759883a49cc36ac6
Jakub Miko <j.miko03@gmail.com>
2024-08-05 17:11:38 +0200

    Added docs for System.h

--- World/System.h
+++ World/System.h
@@ -9,17 +9,30 @@
 #include <TaskSystem.h>
 #include <cmath>
 
+/**
+ * @brief Describes how Entities should be distributed among threads/batches, when submitting them to the TaskSystem
+*/
 struct ComponentCollectionParameters {
-	int num_of_collections;
-	int collection_size;
-	int extra_collections_size;
+	int num_of_collections; ///< number of full sized batches
+	int collection_size; ///< size of full sized batches
+	int extra_collections_size; ///< size of the extra leftover batch
 };
 
+/**
+ * @brief Describes a single batch from the batches submitted to the Task system
+*/
 struct ComponentCollection {
-	int size;
-	int start_index;
+	int size; ///< number of entities in this batch
+	int start_index; ///< index of the first entity this batch should process, it is and index into the system_view_type
 };
 
+/**
+ * @brief Uses the number of entities and threads to produce a ComponentCollectionParameters distribution of the entities into batches
+ * @param size number of entities to be processed
+ * @param num_of_threads number of threads to distribute the batches over
+ * @param min_collection_size minimal number of collections to prevent small amount of entities to be needlessly distributes(since the synchronization overhead would be higher)
+ * @return ComponentCollectionParameters containing the batch distribution
+*/
 static ComponentCollectionParameters GetCollectionsFromSize(size_t size, int num_of_threads, int min_collection_size = 10) {
 	if (size == 0) {
 		return ComponentCollectionParameters{ 0 ,0,0};
@@ -44,12 +57,36 @@ static ComponentCollectionParameters GetCollectionsFromSize(size_t size, int num
 	return ComponentCollectionParameters{ num_of_chunks ,chunk_size,last };
 };
 
+/**
+ * @brief type of an iterator containing all entities with a component
+ * @tparam Component component the entities should have
+*/
 template<typename Component>
 using system_view_type = decltype(std::declval<entt::registry>().view<Component>());
 
+/**
+ * @brief type of an agrregation of Futures holding future values of individual tasks.
+ * @tparam return_type return type expected of each individual task 
+*/
 template<typename return_type>
 using system_return_type = decltype(std::declval<std::vector<Future<return_type*>>>());
 
+/**
+ * @brief Executes a system function on each entity with a component in parallel
+ * 
+ * Template function which abstract complexity of distributing and dispatching a multi-threads ECS system. 
+ * Entities with Component are distributed and processed using the sys_func in parallel using the TaskSystem and their results are then aggregated by a res_func.
+ * 
+ * @tparam Component type of a component which iterated entities need to have
+ * @tparam return_type type which sys_func task returns
+ * @tparam system_function type of sys_func 
+ * @tparam result_function type of res_func
+ * @param world World from which entities are taken
+ * @param sys_func task function which processes entities and returns return_type, takes ComponentCollection, system_view_type<Component> and a entt::registry pointer as arguments
+ * @param res_func task which takes the system_return_type aggregation of all values return by sys_func tasks and processes them as a vector,
+ * takes ComponentCollectionParameters and system_return_type<return_type> reference as arguments
+ * @param min_num_of_tasks_per_thread minimal number of tasks which are going to be dispatched on a thread to prevernt overhead for small number of entities
+*/
 template<typename Component, typename return_type, typename system_function, typename result_function>
 auto RunSystem(World& world, system_function sys_func, result_function res_func, int min_num_of_tasks_per_thread = 1)
 -> decltype((void(),
@@ -108,7 +145,19 @@ auto RunSystem(World& world, system_function sys_func, result_function res_func,
 	res_func(params, lists);
 }
 
-
+/**
+ * @brief Executes a system function on each entity with a component in parallel without aggregating the task output
+ *
+ * Template function which abstract complexity of distributing and dispatching a multi-threads ECS system.
+ * Entities with Component are distributed and processed using the sys_func in parallel using the TaskSystem. 
+ * This version doesn not process task aggregate output.
+ *
+ * @tparam Component type of a component which iterated entities need to have
+ * @tparam system_function type of sys_func
+ * @param world World from which entities are taken
+ * @param sys_func task function which processes entities, takes ComponentCollection, system_view_type<Component> and a entt::registry pointer as arguments
+ * @param min_num_of_tasks_per_thread minimal number of tasks which are going to be dispatched on a thread to prevernt overhead for small number of entities
+*/
 template<typename Component, typename system_function>
 auto RunSystemSimple(World& world, system_function sys_func, int min_num_of_tasks_per_thread = 1)
 -> decltype((void(),
@@ -160,7 +209,19 @@ auto RunSystemSimple(World& world, system_function sys_func, int min_num_of_task
 }
 
 
-
+/**
+ * @brief Executes a system function on each element within a queue in parallel without aggregating the task output
+ *
+ * Template function which abstract complexity of distributing and dispatching a multi-threads ECS system.
+ * Elements in a queue are distributed and processed using the sys_func in parallel using the TaskSystem.
+ * This version doesn not process task aggregate output.
+ *
+ * @tparam Queue type of element the queue contains and this function processes
+ * @tparam system_function type of sys_func
+ * @param world Reference of the current world
+ * @param sys_func task function which processes elements, takes ComponentCollection and std::deque<Queue> reference as arguments
+ * @param min_num_of_tasks_per_thread minimal number of tasks which are going to be dispatched on a thread to prevent overhead for small number of elements
+*/
 template<typename Queue, typename system_function>
 auto RunSystemSimpleQueue(World& world,std::deque<Queue>& queue, system_function sys_func, int min_num_of_tasks_per_thread = 1)
 -> decltype((void(),
@@ -209,7 +270,19 @@ auto RunSystemSimpleQueue(World& world,std::deque<Queue>& queue, system_function
 
 }
 
-
+/**
+ * @brief Executes a system function on each element within a vector in parallel without aggregating the task output
+ *
+ * Template function which abstract complexity of distributing and dispatching a multi-threads ECS system.
+ * Elements in a vector are distributed and processed using the sys_func in parallel using the TaskSystem.
+ * This version doesn not process task aggregate output.
+ *
+ * @tparam Vector type of element the vector contains and this function processes
+ * @tparam system_function type of sys_func
+ * @param world Reference of the current world
+ * @param sys_func task function which processes elements, takes ComponentCollection and std::vector<Vector> reference as arguments
+ * @param min_num_of_tasks_per_thread minimal number of tasks which are going to be dispatched on a thread to prevent overhead for small number of elements
+*/
 template<typename Vector, typename system_function>
 auto RunSystemSimpleVector(World& world, const std::vector<Vector>& vector, system_function sys_func, int min_num_of_tasks_per_thread = 1)
 -> decltype((void(),
