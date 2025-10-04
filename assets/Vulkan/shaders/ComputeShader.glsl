d3b297351bd669c55228b7445979fcbe0eb70284
Jakub Miko <j,miko03@gmail.com>
2025-10-04 22:51:00 +0200

    Fixed material dependencies not generating appropriate write barriers for storage buffers.

--- assets/Vulkan/shaders/ComputeShader.glsl
+++ assets/Vulkan/shaders/ComputeShader.glsl
@@ -24,7 +24,7 @@ layout(std430, set=0, binding = 1) buffer data_buffer
     int numbers[1000];
 };
 
-layout(local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 1000, local_size_y = 1, local_size_z = 1) in;
 
 void main() {
     numbers[0] = 42;
