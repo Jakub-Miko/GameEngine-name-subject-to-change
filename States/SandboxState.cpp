3c30148041a808fad3b67a3b29864374f272630a
Jakub Miko <j,miko03@gmail.com>
2025-10-05 17:01:14 +0200

    Added a test for one threadblock prefix sum.

--- States/SandboxState.cpp
+++ States/SandboxState.cpp
@@ -3,7 +3,7 @@
 #include <TestLayer.h>
 #include <FileManager.h>
 
-#include "TestLayers/OneThreadblockReduction.h"
+#include "TestLayers/OneThreadBlockPrefixSum.h"
 
 SandboxState::SandboxState() : GameState(), m_TestLayer(nullptr) {
 
@@ -22,7 +22,7 @@ bool SandboxState::OnEvent(Event* e)
 
 void SandboxState::OnAttach()
 {
-	m_TestLayer = new OneThreadblockReduction();
+	m_TestLayer = new OneThreadBlockPrefixSum();
 }
 
 void SandboxState::OnDeattach()
