b98656818c385c1f533357a1dc34a779a8167bb0
Jakub Miko <j,miko03@gmail.com>
2025-10-06 14:38:20 +0200

    Optimized the prefix sum down to 0.1ms per million uint32_t items on rtx 3070ti laptop.

--- TestLayers/MultiThreadBlockPrefixSum.cpp
+++ TestLayers/MultiThreadBlockPrefixSum.cpp
@@ -7,23 +7,22 @@
 #include "Renderer/Renderer.h"
 #include "Renderer/RenderResourceManager.h"
 
-#define NUM_OF_THREADBLOCKS 10
-#define NUM_OF_THREADS_IN_THREADBLOCK 1000
+#define NUM_OF_THREADBLOCKS 2000
+#define NUM_OF_THREADS_IN_THREADBLOCK 512
 #define NUM_OF_THREADS NUM_OF_THREADBLOCKS * NUM_OF_THREADS_IN_THREADBLOCK
 
-struct StateBlock {
-    uint32_t state_flag;
-    uint32_t reduction;
-    uint32_t prefix;
-};
 
 #define STATE_BUFFER_SIZE NUM_OF_THREADBLOCKS * sizeof(StateBlock) + sizeof(uint32_t)
 
-MultiThreadBlockPrefixSum::MultiThreadBlockPrefixSum() {
-    ComputePipelineDescriptor descriptor = {};
-    descriptor.shader = ShaderManager::Get()->GetShader("compute/MultiThreadblockPrefixSum.glsl");
-    pipeline = PipelineManager::Get()->CreatePipeline(descriptor);
+MultiThreadBlockPrefixSum::~MultiThreadBlockPrefixSum() {
 
+}
+
+MultiThreadBlockPrefixSum::MultiThreadBlockPrefixSum() {
+    input.reset((new int[NUM_OF_THREADS] ()));
+    output.reset((new int[NUM_OF_THREADS] ()));
+    validate.reset((new int[NUM_OF_THREADS] ()));
+    state.reset((new StateBlock[NUM_OF_THREADBLOCKS] ()));
     RenderBufferDescriptor buffer_desc = {};
     buffer_desc.buffer_size = NUM_OF_THREADS * sizeof(int);
     buffer_desc.type = RenderBufferType::DEFAULT;
@@ -42,6 +41,33 @@ MultiThreadBlockPrefixSum::MultiThreadBlockPrefixSum() {
 
     state_buffer = RenderResourceManager::Get()->CreateBuffer(state_buffer_desc);
 
+    std::random_device generator;
+    std::mt19937 rng(generator());
+    std::uniform_int_distribution<int> distribution(0,10);
+    int counter = 0;
+    for(int i = 0; i < NUM_OF_THREADS; i++) {
+        auto num = distribution(rng);
+        input[i] = num;
+        validate[i] = counter;
+        counter += num;
+    }
+
+    auto list = Renderer::Get()->GetRenderCommandList();
+    int zero = 0;
+    RenderResourceManager::Get()->UploadDataToBuffer(list, output_buffer,output.get(), sizeof(int) * NUM_OF_THREADS,0);
+    RenderResourceManager::Get()->UploadDataToBuffer(list, input_buffer,input.get(), sizeof(int) * NUM_OF_THREADS,0);
+    RenderResourceManager::Get()->UploadDataToBuffer(list, validation_buffer,validate.get(), sizeof(int) * NUM_OF_THREADS,0);
+    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,&zero, sizeof(int),0);
+    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,state.get(), sizeof(StateBlock) * NUM_OF_THREADBLOCKS,sizeof(int));
+
+    Renderer::Get()->GetCommandQueue()->ExecuteRenderCommandList(list);
+
+
+    ComputePipelineDescriptor descriptor = {};
+    descriptor.shader = ShaderManager::Get()->GetShader("compute/MultiThreadblockPrefixSum.glsl");
+    pipeline = PipelineManager::Get()->CreatePipeline(descriptor);
+
+
     auto mat_template = MaterialManager::Get()->GetMaterialTemplate("MultiThreadblockPrefixSumSettings");
     material = mat_template->CreateMaterial();
     material->SetParameter("buffer_size", 1000);
@@ -55,30 +81,9 @@ MultiThreadBlockPrefixSum::MultiThreadBlockPrefixSum() {
 void MultiThreadBlockPrefixSum::OnUpdate(float delta_time) {
     std::cout << "Running Compute\n";
     auto list = Renderer::Get()->GetRenderCommandList();
-
-
-    int random_nums[NUM_OF_THREADS] = {};
-    StateBlock state_block[NUM_OF_THREADBLOCKS] = {};
-
-    RenderResourceManager::Get()->UploadDataToBuffer(list, output_buffer,&random_nums, sizeof(int) * NUM_OF_THREADS,0);
-    RenderResourceManager::Get()->UploadDataToBuffer(list, validation_buffer,&random_nums, sizeof(int) * NUM_OF_THREADS,0);
-    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,&random_nums, sizeof(int),0);
-    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,&state_block, sizeof(StateBlock) * NUM_OF_THREADBLOCKS,sizeof(int));
-
-    std::random_device generator;
-    std::mt19937 rng(generator());
-    std::uniform_int_distribution<int> distribution(0,10);
-    int prefix_sum[NUM_OF_THREADS] = {};
-    int counter = 0;
-    for(int i = 0; i < NUM_OF_THREADS; i++) {
-        auto num = distribution(rng);
-        random_nums[i] = num;
-        prefix_sum[i] = counter;
-        counter += num;
-    }
-
-    RenderResourceManager::Get()->UploadDataToBuffer(list, input_buffer,&random_nums, sizeof(int) * NUM_OF_THREADS,0);
-    RenderResourceManager::Get()->UploadDataToBuffer(list, validation_buffer,&prefix_sum, sizeof(int) * NUM_OF_THREADS,0);
+    int zero = 0;
+    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,&zero, sizeof(int),0);
+    RenderResourceManager::Get()->UploadDataToBuffer(list, state_buffer,state.get(), sizeof(StateBlock) * NUM_OF_THREADBLOCKS,sizeof(int));
 
     list->SetPipeline(pipeline);
     list->SetMaterial("MultiThreadblockPrefixSumSettings",material);
@@ -87,3 +92,5 @@ void MultiThreadBlockPrefixSum::OnUpdate(float delta_time) {
 
     Renderer::Get()->GetCommandQueue()->ExecuteRenderCommandList(list);
 }
+
+
--- TestLayers/MultiThreadBlockPrefixSum.h
+++ TestLayers/MultiThreadBlockPrefixSum.h
@@ -4,11 +4,20 @@
 #include "Layer.h"
 #include "Renderer/PipelineManager.h"
 
+struct StateBlock {
+    uint32_t state_flag;
+};
+
 class MultiThreadBlockPrefixSum : public Layer {
 public:
+    ~MultiThreadBlockPrefixSum() override;
     MultiThreadBlockPrefixSum();
     void OnUpdate(float delta_time) override;
 private:
+    std::unique_ptr<int[]> input;
+    std::unique_ptr<int[]> output;
+    std::unique_ptr<int[]> validate;
+    std::unique_ptr<StateBlock[]> state;
     std::shared_ptr<Pipeline> pipeline;
     std::shared_ptr<Material> material;
     std::shared_ptr<RenderBufferResource> input_buffer;
